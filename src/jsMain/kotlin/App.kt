3daa1a0a2cb77fa6492be6b7aebacf390bd68996
BeckMikkelsen <nikolajbeckmikkelsen@outlook.com>
2022-11-23 20:34:42 +0100

    style

--- src/jsMain/kotlin/App.kt
+++ src/jsMain/kotlin/App.kt
@@ -1,7 +1,11 @@
+import emotion.react.css
+import csstype.*
 import react.*
 import kotlinx.coroutines.*
+import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.h1
 import react.dom.html.ReactHTML.li
+import react.dom.html.ReactHTML.p
 import react.dom.html.ReactHTML.ul
 
 private val scope = MainScope()
@@ -15,30 +19,39 @@ val App = FC<Props> {
         }
     }
 
-    h1 {
-        +"Christmas wish list"
-    }
+    div {
+        h1 {
+            css {
+                display = Display.flex
+                justifyContent = JustifyContent.spaceAround
+            }
+            +"Christmas wish list"
+        }
 
-    ul {
-        shoppingList.sortedByDescending(ShoppingListItem::priority).forEach { item ->
-            li {
-                key = item.toString()
-                onClick = {
-                    scope.launch {
-                        deleteShoppingListItem(item)
-                        shoppingList = getShoppingList()
+        ul {
+            shoppingList.sortedByDescending(ShoppingListItem::priority).forEach { item ->
+                li {
+                    key = item.toString()
+                    onClick = {
+                        scope.launch {
+                            deleteShoppingListItem(item)
+                            shoppingList = getShoppingList()
+                        }
                     }
+                    +"[${item.priority}] ${item.desc} "
                 }
-                +"[${item.priority}] ${item.desc} "
             }
         }
-    }
-    inputComponent {
-        onSubmit = { input ->
-            val cartItem = ShoppingListItem(input.replace("!", ""), input.count { it == '!' })
-            scope.launch {
-                addShoppingListItem(cartItem)
-                shoppingList = getShoppingList()
+        p {
+            +"Add item to wish list (insert ! to set priority). Press item to delete from list"
+        }
+        inputComponent {
+            onSubmit = { input ->
+                val cartItem = ShoppingListItem(input.replace("!", ""), input.count { it == '!' })
+                scope.launch {
+                    addShoppingListItem(cartItem)
+                    shoppingList = getShoppingList()
+                }
             }
         }
     }
